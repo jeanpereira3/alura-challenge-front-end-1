f617bb9bdd6931452eb9299f698aa185e0738869
jeanpereira3 <jeansilv13@gmail.com>
2023-04-17 12:43:08 -0300

    opcao de exportar codigo

--- js/exportar.js
+++ js/exportar.js
@@ -2,12 +2,12 @@ const codigoEditorPrincipal = document.querySelector('[data-editor=editor-princi
 
 const botaoExportar = document.querySelector('#exportar')
 
-const exportar = (formato) => {
+const exportarImagem = (formato) => {
 
-  html2canvas(codigoEditorPrincipal).then((canvas)=>{
+  html2canvas(codigoEditorPrincipal).then((canvas) => {
     const image = canvas.toDataURL(`image/${formato}`)
     var link = document.createElement('a')
-    link.href = image 
+    link.href = image
     link.download = `meu-codigo.${formato}`
     link.click()
     link.remove()
@@ -15,9 +15,33 @@ const exportar = (formato) => {
 
 }
 
+const exportarCodigo = () => {
+  const linguagemExportacao = document.querySelector('#texto_select').value
+  const codigoTexto = document.querySelector('code').innerHTML
+  const arquivo = new Blob([codigoTexto], { type: `text/${linguagemExportacao}` });
+  const url = URL.createObjectURL(arquivo);
+
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = `meu-arquivo.${formatoCodigo[linguagemExportacao]}`;
+  link.click();
+
+  URL.revokeObjectURL(url);
+
+}
+
 botaoExportar.addEventListener('change', () => {
   const formato = document.querySelector('#exportar').value
-  if (formato === 'png' || formato === 'jpg' || formato === 'svg'){
-    exportar(formato)
+  if (formato === 'png' || formato === 'jpg') {
+    exportarImagem(formato)
+  }
+
+  if (formato === 'codigo') {
+    exportarCodigo(formato)
   }
 })
+
+const formatoCodigo = {
+  'javascript': 'js',
+  'html': 'html'
+}
